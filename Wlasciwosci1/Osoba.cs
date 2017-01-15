c0c0c6581dc1de7e3e7e88f06167d0a25deaf80e
Piotrekk85 <piotr.jastr@gmail.com>
2017-01-15 14:53:41 +0100

    Add files via upload

---
+++ Wlasciwosci1/Osoba.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wlasciwosci1
+{
+    class Osoba
+    {
+        private int wiek; //pole
+        public int Wiek //właściwość
+        {
+            get { return wiek; }
+            set { wiek = value; }
+        }
+
+        
+    }
+}
---
+++ Wlasciwosci1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wlasciwosci1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba osoba1 = new Osoba();
+            osoba1.Wiek = 21;  //set
+            Console.WriteLine(osoba1.Wiek); //get
+            Console.ReadKey();
+
+
+        }
+    }
+}
